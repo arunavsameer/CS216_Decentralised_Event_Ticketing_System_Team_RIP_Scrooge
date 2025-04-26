f0cbcc76614517f7ab38edfa41786653b650e37c
Arunav <arunavsameer@gmail.com>
2025-04-27 01:51:20 +0530

    updated organizer UI

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
 import EventFactoryArtifact from "../abis/EventFactory.json";
 
 export const NETWORK_URL     = "http://localhost:8545";
-export const FACTORY_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const FACTORY_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 export const FACTORY_ABI     = EventFactoryArtifact.abi;
